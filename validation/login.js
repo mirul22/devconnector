082352ac4b7b8f5fa2ca417a9500571fddfb16d5
mirul22 <amirulikmal.biz@gmail.com>
2019-02-24 20:53:35 +0800

    Profile API Routes - 1

--- validation/login.js
+++ validation/login.js
@@ -1,7 +1,7 @@
 const isEmpty = require("./is-empty");
 const Validator = require("validator");
 
-module.exports = function validateLognInput(data) {
+module.exports = function validateLoginInput(data) {
   let errors = {};
 
   data.email = !isEmpty(data.email) ? data.email : "";
