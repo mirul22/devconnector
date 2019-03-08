60a8273c163715c5af901687f52f3c25422c2d68
mirul22 <amirulikmal.biz@gmail.com>
2019-03-09 04:34:47 +0800

    keys-dev

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector',
+  mongoURI: 'mongodb://admin:admin123@tmcluster-shard-00-00-ym7ou.mongodb.net:27017,tmcluster-shard-00-01-ym7ou.mongodb.net:27017,tmcluster-shard-00-02-ym7ou.mongodb.net:27017/test?ssl=true&replicaSet=TMCluster-shard-0&authSource=admin&retryWrites=true',
   secretOrKey: 'secret'
 };
