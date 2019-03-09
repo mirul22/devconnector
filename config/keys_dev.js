990659f2ce8d9177e37389707e2b76ebedcd1b6f
Amirul Ikmal <amirulikmal.biz@gmail.com>
2019-03-09 13:06:55 +0800

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://admin:admin123@tmcluster-shard-00-00-ym7ou.mongodb.net:27017,tmcluster-shard-00-01-ym7ou.mongodb.net:27017,tmcluster-shard-00-02-ym7ou.mongodb.net:27017/test?ssl=true&replicaSet=TMCluster-shard-0&authSource=admin&retryWrites=true",
-  secretOrKey: "secret"
-};
